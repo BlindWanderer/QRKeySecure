8e4484fa3e5618f1af8cef985aa9382309578ecb
Strife Onizuka <blindwanderer@gmail.com>
2013-04-30 02:33:10 -0400

    Those are instance fields!

--- src/edu/mccc/cos210/qrks/qrcode/Point.java
+++ src/edu/mccc/cos210/qrks/qrcode/Point.java
@@ -1,8 +1,8 @@
 package edu.mccc.cos210.qrks.qrcode;
 
 public class Point {
-	public static int x;
-	public static int y;
+	public int x;
+	public int y;
 	public Point(int x, int y){
 		this.x = x;
 		this.y = y;
