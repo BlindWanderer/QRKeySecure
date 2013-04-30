02073d4efc4cffa26818261c7294268d1fbb09f4
Strife Onizuka <blindwanderer@gmail.com>
2013-04-30 18:15:27 -0400

    working on verification during finding

--- src/edu/mccc/cos210/qrks/QRReader.java
+++ src/edu/mccc/cos210/qrks/QRReader.java
@@ -10,8 +10,18 @@ import java.io.*;
 import javax.imageio.*;
 
 public class QRReader implements Reader<BufferedImage, BufferedImage> {
-	
-	public java.util.List<Item<BufferedImage>> process(BufferedImage input) {
+	private class Match {
+		public final int start;
+		public final int end;
+		public Match(final int start, final int end) {
+			this.start = start;
+			this.end = end;
+		}
+		public int getCenter() {
+			return (start + end) / 2;
+		}
+	}
+	public List<Item<BufferedImage>> process(BufferedImage input) {
 		int width = input.getWidth();
 		int height = input.getHeight();
 		int[] data = new int[(width) * (height)];
@@ -49,36 +59,13 @@ public class QRReader implements Reader<BufferedImage, BufferedImage> {
 		//not a hundered percent sure this is the order it goes in!
 
 		for (int y = 0, p = 0; y < height; y++) {
-			int v1 = 0;
-			int v2 = 0;
-			int v3 = 0;
-			int v4 = 0;
-			int c = 0;
-			int total = 0;
-			int x = 0;
-			for (int v0 : runLengthEncode(bw, p, p += height)) {
-				c++;
-				x += v0;
-				total += v0;
-				if (c > 4) {
-					int t1 = total / 14;
-					int t3 = (total * 3) / 14;
-					int t5 = (total * 5) / 14;
-					int t7 = total / 2;
-					if ((v0 >= t1) && (v0 <=t3) &&
-						(v1 >= t1) && (v1 <=t3) &&
-						(v2 >= t5) && (v2 <=t7) &&
-						(v3 >= t1) && (v3 <=t3) &&
-						(v4 >= t1) && (v4 <=t3)) {
-						//starts at x-total
-						//yeah ^_^
+			for (Match m : process(bw, p, p+=height, 1)) {
+				int width = m.end - m.start;
+				for (Match w : process(bw, m.center() % height, width * height, height)) {
+					for (Match w : process(bw, m.center(), width * height, height+1)) {
+						
 					}
 				}
-				total -= v4;
-				v4 = v3;
-				v3 = v2;
-				v2 = v1;
-				v1 = v0;
 			}
 		}
 		return null;
@@ -98,13 +85,16 @@ public class QRReader implements Reader<BufferedImage, BufferedImage> {
 		return (min + max) / 2;
 	}
 	private static List<Integer> runLengthEncode(boolean [] bw) {
-		return runLengthEncode(bw, 0, bw.length);
+		return runLengthEncode(bw, 0, bw.length, 1);
 	}
 	private static List<Integer> runLengthEncode(final boolean [] bw, final int start /*inclusive*/, final int end /*exclusive*/) {
+		return runLengthEncode(bw, start, end, 1);
+	}
+	private static List<Integer> runLengthEncode(final boolean [] bw, final int start /*inclusive*/, final int end /*exclusive*/, int stride) {
 		List<Integer> rle = new LinkedList<Integer>();
 		int c = 1;
 		boolean b = bw[start];
-		for (int p = start + 1; p < end; p++) {
+		for (int p = start + stride; p < end; p+= stride) {
 			if (bw[p] != b) {
 				rle.add(c);
 				b = !b;
@@ -113,5 +103,35 @@ public class QRReader implements Reader<BufferedImage, BufferedImage> {
 		}
 		rle.add(c);
 		return rle;
+	}
+	private static List<Match> process(final boolean [] bw, final int start, final int end, int stride) {
+		int v1 = 0;
+		int v2 = 0;
+		int v3 = 0;
+		int v4 = 0;
+		int c = 0;
+		int total = 0;
+		int x = 0;
+		List<Match> matches = new LinkedList<>();
+		for (int v0 : runLengthEncode(bw, start, end, stride)) {
+			total -= v4;
+			v4 = v3;
+			v3 = v2;
+			v2 = v1;
+			v1 = v0;
+			c++;
+			x += v0;
+			total += v0;
+			if (c > 4) {
+				int t1 = total / 14;
+				int t3 = (total * 3) / 14;
+				int t5 = (total * 5) / 14;
+				int t7 = total / 2;
+				if ((v0 >= t1) && (v0 <=t3) && (v1 >= t1) && (v1 <=t3) && (v2 >= t5) && (v2 <=t7) && (v3 >= t1) && (v3 <=t3) && (v4 >= t1) && (v4 <=t3)) {
+					matches.add(new Match(x - total, x));
+				}
+			}
+		}
+		return matches;
 	}
 }
