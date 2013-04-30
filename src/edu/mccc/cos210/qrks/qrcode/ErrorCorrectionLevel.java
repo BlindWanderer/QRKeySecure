f1048731915898b416b915ef946f0cab25bd710a
Cayte <Cayte@hotmail.com>
2013-04-30 02:59:13 -0400

    .

--- src/edu/mccc/cos210/qrks/qrcode/ErrorCorrectionLevel.java
+++ src/edu/mccc/cos210/qrks/qrcode/ErrorCorrectionLevel.java
@@ -4,22 +4,32 @@ import javax.swing.*;
 import java.awt.*;
 
 public enum ErrorCorrectionLevel {
-	L(7),
-	M(15),
-	Q(25),
-	H(30);
-	private final int percentage;
-	private ErrorCorrectionLevel(final int percentage) {
-		this.percentage = percentage;
+	L(0, 7),
+	M(1, 15),
+	Q(2, 25),
+	H(3, 30);
+	private static ErrorCorrectionLevel[] ecls;
+	static {
+		ErrorCorrectionLevel[] t = ErrorCorrectionLevel.values();
+		ecls = new ErrorCorrectionLevel[t.length];
+		for (ErrorCorrectionLevel ecl : t) {
+			ecls[ecl.index] = ecl;
+		}
 	}
-	public int getPercentage() {
-		return percentage;
+	public final int index;
+	public final int percentage;
+	private ErrorCorrectionLevel(final int index, final int percentage) {
+		this.index = index;
+		this.percentage = percentage;
 	}
 	public String toString() {
 		return getName() + " ~ " + percentage + "%";
 	}
 	public String getName() {
 		return super.toString();
+	}
+	static ErrorCorrectionLevel parseIndex(final int index) {
+		return ecls[index];
 	}
 	/*
 	public static void main(String[] args) {
